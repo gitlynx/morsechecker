2f0aea5434d1c2ca17b7c7a8116a04b3b5179052
Jan Cornelis <Jan.Cornelis@gmail.com>
2024-08-18 18:44:20 +0200

    First minimalistic example

--- morse-checker.py
+++ morse-checker.py
@@ -4,6 +4,21 @@
 #   https://pygamewidgets.readthedocs.io/en/latest/widgets/progressbar/
 #   https://pygamewidgets.readthedocs.io/en/latest/widgets/slider/
 
+"""
+    This is a simple visual representation of morse code linked to an 
+    (external) input. Showing the dots and dashes.
+
+    from https://en.wikipedia.org/wiki/Morse_code:
+    A morse code symbol consists of 'dits' and 'dahs'. The dit duration can
+    vary for signal clarity and operator skills, but it's the basic unit
+    of time measurement.
+
+    - A 'dah' is minimal 3 times the duration of a 'dit'.
+    - There's minimal 1 'dit' space between consecutive 'dits' and 'dahs' to 
+      separate morse symbols
+    - There is minimal 3 'dit' space between letters.
+    - There is minimal 7 'dit' space between words.
+"""
 
 
 
@@ -37,9 +52,30 @@ def morse_grid(surface: pygame.Surface) -> ():
 
 
 def render_reference(surface: pygame.Surface, dits_array, y_offset, character = None):
+    '''
+        Render a dits reference (a dit is the 
+    '''
+    dit_pattern = [False for element in range(32)]
+
+    if character is not None and character in Morse.morse_dict:
+
+#        import pdb
+#        pdb.set_trace()
+
+        morse_pattern = Morse.morse_dict[character]
+        for i in range(len(morse_pattern)):
+            dit_pattern[i + 12] = True if morse_pattern[i] is True else False
+
+    print(f"Dit Pattern: {dit_pattern}")
 
     for dit in dits_array:
-        pygame.draw.rect(surface, (176, 176, 176), dit.move(0, y_offset), border_radius=3, width=2)
+        if character is not None and character in Morse.morse_dict:
+            pattern_index = dits_array.index(dit)
+            rec_width = 0 if dit_pattern[pattern_index] is True else 2
+        else:
+            rec_width = 2
+
+        pygame.draw.rect(surface, (176, 176, 176), dit.move(0, y_offset), border_radius=3, width=rec_width)
 
 def render_marker(surface: pygame.Surface, y_offset, startTime, maxTime, current_time, on: bool) -> bool:
     '''
@@ -58,6 +94,8 @@ def render_marker(surface: pygame.Surface, y_offset, startTime, maxTime, current
     return False
 
 def game():
+    morse_symbol = None
+
     #Pygame setup
     pygame.init()
     win = pygame.display.set_mode((1000,600))
@@ -81,8 +119,9 @@ def game():
                 pygame.quit()
                 run = False
                 quit()
-#            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-#                paddle_press = True
+            if event.type == pygame.KEYDOWN:
+                user_text = event.unicode
+                morse_symbol = user_text[0]
 
         keys_pressed = pygame.key.get_pressed()
         paddle_press = keys_pressed[pygame.K_SPACE]
@@ -90,7 +129,7 @@ def game():
         if render_marker(win, 60, startTime, 1.0, time.time(), paddle_press) is False:
             startTime = time.time()
             win.fill(DISPLAY_COLOR)
-            render_reference(win, dits_array, 20)
+            render_reference(win, dits_array, 20, character = morse_symbol)
 
     
         pygame.display.update()
